ed0e2ab4580d16a355860fa74576abdc05f9cb28
IlyaPesterev <ya.hhhh.ya@mail.ru>
2024-07-28 22:13:35 +0200

    task completed

--- main.go
+++ main.go
@@ -1,41 +1,63 @@
 package main
 
 import (
-	"fmt"
+    "database/sql"
+    "fmt"
 
-	_ "modernc.org/sqlite"
+    _ "modernc.org/sqlite"
 )
 
 type Sale struct {
-	Product int
-	Volume  int
-	Date    string
+    Product int
+    Volume  int
+    Date    string
 }
 
 // String реализует метод интерфейса fmt.Stringer для Sale, возвращает строковое представление объекта Sale.
 // Теперь, если передать объект Sale в fmt.Println(), то выведется строка, которую вернёт эта функция.
 func (s Sale) String() string {
-	return fmt.Sprintf("Product: %d Volume: %d Date:%s", s.Product, s.Volume, s.Date)
+    return fmt.Sprintf("Product: %d Volume: %d Date:%s", s.Product, s.Volume, s.Date)
 }
 
 func selectSales(client int) ([]Sale, error) {
-	var sales []Sale
+    db, err := sql.Open("sqlite", "demo.db")
+    if err != nil {
+        return nil, err
+    }
+    defer db.Close()
 
-	// напишите код здесь
+    var sales []Sale
 
-	return sales, nil
+    rows, err := db.Query("SELECT product, volume, date FROM sales WHERE client = :client", sql.Named("client", client))
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        sale := Sale{}
+
+        err := rows.Scan(&sale.Product, &sale.Volume, &sale.Date)
+        if err != nil {
+            return nil, err
+        }
+
+        sales = append(sales, sale)
+    }
+
+    return sales, nil
 }
 
 func main() {
-	client := 208
+    client := 208
 
-	sales, err := selectSales(client)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+    sales, err := selectSales(client)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
 
-	for _, sale := range sales {
-		fmt.Println(sale)
-	}
+    for _, sale := range sales {
+        fmt.Println(sale)
+    }
 }
